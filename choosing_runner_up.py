6cfb8cdd83d79d3a84ca26bcbab08e531faf3243
chandrashekarumapathi <chandrashekarumapathi@gmail.com>
2021-03-21 13:34:18 +0100

    change functionality

--- choosing_runner_up.py
+++ choosing_runner_up.py
@@ -1,17 +1,18 @@
 my_list = []
 n = int(input('Enter numbers: '))
 arr = list(map(int, input('Enter the number to split: ').split()))
-z = max(arr)
-while max(arr) == z:
-    arr.remove(max(arr))
-
-print(max(arr))
-
-"""
 for i in arr:
     my_list.append(i)
 my_list = list(dict.fromkeys(my_list))
 my_list.sort()
 print(my_list[-2])
+
+
+"""
+z = max(arr)
+while max(arr) == z:
+    arr.remove(max(arr))
+
+print(max(arr))
 """
 
